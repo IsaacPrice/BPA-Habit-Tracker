def4c5942370ee8d88a7d09cd99a4daabc4294c2
Isaac Price <100382457+IsaacPrice@users.noreply.github.com>
2024-10-08 16:42:03 -0400

    Create react-template

---
+++ react-app/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+import Home from './pages/Home';
+
+
+
+function App() 
+{
+	return (
+		<Home />
+	);
+}
+
+export default App;
---
+++ react-app/src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
+
+const Home: React.FC = () => 
+{
+	return (
+		<Box>
+			<Typography variant="h1">Homepage</Typography>
+		</Box>
+	);
+}
+
+
+export default Home;
